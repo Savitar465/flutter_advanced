a511ddcdc7b79afd0b6e17754aa31fb51dc94571
Savitar <jonasmaidana47@gmail.com>
2025-08-29 02:10:05 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced/singleton.dart';
+import 'package:flutter_advanced/mixin.dart';
+import 'package:flutter_advanced/snackbar_mixin.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +11,150 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Music Player Demo',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with LoggingMixin, SnackbarMixin<MyHomePage> {
+  final MusicPlayer _musicPlayer = MusicPlayer.instance;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _musicPlayer.addListener(() {
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Music Player'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _musicPlayer.playlist.length,
+              itemBuilder: (context, index) {
+                final songTitle = _musicPlayer.playlist[index].split('/').last;
+                return ListTile(
+                  title: Text(songTitle),
+                  leading: Icon(
+                    _musicPlayer.currentSongIndex == index &&
+                            _musicPlayer.playerState == PlayerState.playing
+                        ? Icons.pause_circle_filled
+                        : Icons.play_circle_filled,
+                  ),
+                  tileColor: _musicPlayer.currentSongIndex == index
+                      ? Colors.black45
+                      : null,
+                  onTap: () {
+                    if (_musicPlayer.currentSongIndex == index &&
+                        _musicPlayer.playerState == PlayerState.playing) {
+                      _musicPlayer.pause();
+                      final message = 'Music paused';
+                      log(message);
+                      showSnackbar(message);
+                    } else {
+                      _musicPlayer.play(index);
+                      final message = 'Playing $songTitle';
+                      log(message);
+                      showSnackbar(message);
+                    }
+                  },
+                );
+              },
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(100.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  _musicPlayer.currentSong?.split('/').last ?? 'No song selected',
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.skip_previous),
+                      iconSize: 48,
+                      onPressed: () {
+                        _musicPlayer.playPrevious();
+                        final message = 'Playing previous song';
+                        log(message);
+                        showSnackbar(message);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(_musicPlayer.playerState == PlayerState.playing
+                          ? Icons.pause
+                          : Icons.play_arrow),
+                      iconSize: 64,
+                      onPressed: () {
+                        if (_musicPlayer.playerState == PlayerState.playing) {
+                          _musicPlayer.pause();
+                          final message = 'Music paused';
+                          log(message);
+                          showSnackbar(message);
+                        } else {
+                          _musicPlayer.play();
+                          final message = 'Music playing';
+                          log(message);
+                          showSnackbar(message);
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.skip_next),
+                      iconSize: 48,
+                      onPressed: () {
+                        _musicPlayer.playNext();
+                        final message = 'Playing next song';
+                        log(message);
+                        showSnackbar(message);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Text('Volume'),
+                Slider(
+                  value: _musicPlayer.volume,
+                  onChanged: (value) {
+                    _musicPlayer.setVolume(value);
+                    // No snackbar here to avoid spamming
+                  },
+                  onChangeEnd: (value) {
+                    final message = 'Volume changed to ${value.toStringAsFixed(2)}';
+                    log(message);
+                    showSnackbar(message);
+                  },
+                ),
+
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
